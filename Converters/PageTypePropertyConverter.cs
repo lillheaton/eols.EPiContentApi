f7d3d67e4cd29510e3c5c88ab017d788b7b0f716
Emil Olsson <emilolsson93@gmail.com>
2015-12-22 11:48:31 +0100

    New property converter

---
+++ Converters/PageTypePropertyConverter.cs
@@ -0,0 +1,24 @@
+﻿using EOls.EPiContentApi.Interfaces;
+using EPiServer.DataAbstraction;
+
+namespace EOls.EPiContentApi.Converters
+{
+    public class PageTypePropertyConverter : IApiPropertyConverter<PageType>
+    {
+        public object Convert(PageType obj, string locale)
+        {
+            if (obj == null) return null;
+            return
+                new
+                {
+                    obj.ID,
+                    obj.Name,
+                    obj.FullName,
+                    obj.ModelType,
+                    obj.DefaultMvcController,
+                    obj.DefaultMvcPartialView,
+                    obj.DefaultWebFormTemplate
+                };
+        }
+    }
+}
